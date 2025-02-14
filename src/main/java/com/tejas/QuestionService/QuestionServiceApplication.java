6ad662f4a350652428e8518d251ef91f08a8b5b3
Tejas <tejaskatke900a@gmail.com>
2025-02-15 01:35:09 +0530

    Add: Comment

--- src/main/java/com/tejas/QuestionService/QuestionServiceApplication.java
+++ src/main/java/com/tejas/QuestionService/QuestionServiceApplication.java
@@ -7,7 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class QuestionServiceApplication {
-
+//	My name is Sanket
 	public static void main(String[] args) {
 		SpringApplication.run(QuestionServiceApplication.class, args);
 	}
